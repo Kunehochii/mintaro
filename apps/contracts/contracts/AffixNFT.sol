5e356a5412e2feeb8d661879d1559389ed83d349
Carlow18 <johncarlo.macoco-16@cpu.edu.ph>
2026-05-04 00:43:37 +0800

    feat: add mint price management in AffixNFT contract

--- apps/contracts/contracts/AffixNFT.sol
+++ apps/contracts/contracts/AffixNFT.sol
@@ -6,18 +6,27 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract AffixNFT is ERC721URIStorage, Ownable {
   uint256 private _nextTokenId;
+  uint256 public mintPrice = 0.01 ether;
 
   event MintRequested(uint256 indexed tokenId, address indexed minter, uint256 seed);
+  event MintPriceUpdated(uint256 oldPrice, uint256 newPrice);
 
   constructor(address initialOwner) ERC721('AffixNFT', 'AFFIX') Ownable(initialOwner) {}
 
   function mint() external payable {
+    require(msg.value == mintPrice, 'Incorrect ETH amount');
     uint256 tokenId = _nextTokenId++;
     uint256 seed = block.prevrandao;
 
     _safeMint(msg.sender, tokenId);
 
     emit MintRequested(tokenId, msg.sender, seed);
+  }
+
+  function setMintPrice(uint256 _newPrice) external onlyOwner {
+    uint256 oldPrice = mintPrice;
+    mintPrice = _newPrice;
+    emit MintPriceUpdated(oldPrice, _newPrice);
   }
 
   function setTokenURI(uint256 tokenId, string calldata uri) external onlyOwner {
